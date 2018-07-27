e58f2b1b7e869f71f1638e61a09e7c1f29500047
Javier Zapata <zapataj87@gmail.com>
2018-07-27 17:43:05 -0500

    able to add content and edit content that has already been submitted

--- src/App.js
+++ src/App.js
@@ -9,35 +9,46 @@ import FormType from './FormType.js'
 class App extends Component {
 
   state = {
+    id: 0,
     sections: [],
-    data: [
-      {'header': "this is a header"},
-      {'p': "paragraph"}
-    ]
+    data: [],
   }
+
 
   headerFunction = data => {
       return (
-        <h1>{data}</h1>
+        <h1 key={this.state.id}>{data}</h1>
       )
   }
 
   paragraphFunction = data => {
     return (
 
-      <p>{data}</p>
+      <p key={this.state.id}>{data}</p>
     )
+  }
+
+  updateCurrentData = (type, data, id) => {
+    console.log(this.state.data)
+    console.log(data, 'inside updateCurrentData', ' this is the id ' , id)
+
+    let newState = [...this.state.data];
+    console.log('this is the newstate ', newState)
+    newState[id] = {[type]: data};
+
+    this.setState({data: newState})
   }
 
   //Function that returns a <FornType> component
   handleClick = () => {
-    this.setState({sections: [...this.state.sections, <FormType addDataToState={this.addDataToState}/>]})
+    this.setState({sections: [...this.state.sections, <FormType key={this.state.id} id={this.state.id} updateCurrentData={this.updateCurrentData} addDataToState={this.addDataToState}/>]})
+    this.setState({id: this.state.id + 1})
   }
 
-
+  // helps in rendering obj/json data to render
   tagFunctionSelector = (key, value) => {
     switch (key) {
-      case 'header':
+      case 'h1':
         return this.headerFunction(value)
       case 'p':
         return this.paragraphFunction(value)
@@ -49,6 +60,7 @@ class App extends Component {
     }
   }
 
+  // this is adding data to obj/json so that it may be read in the future
   addDataToState = (key, value) => {
     console.log(key, value, " inside add datatostate")
     this.setState({data: [...this.state.data, {[key]: value}]})
@@ -81,8 +93,9 @@ class App extends Component {
           To get startd, edit <code>src/App.js</code> and save to reload.
         </p>
         <button onClick={this.handleClick}>Add More</button>
-        <FormType addDataToState={this.addDataToState}/>
+
         {this.state.sections}
+        <h1>Blog View</h1>
         {
           blogItems
         }
--- src/FormType.js
+++ src/FormType.js
@@ -8,7 +8,8 @@ class FormType extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      htmltype: 'header',
+      submitted: false,
+      htmltype: 'h1',
       data: '',
     }
 
@@ -23,26 +24,37 @@ class FormType extends React.Component {
   }
 
   handleSubmit = (event) => {
+    if (this.state.submitted) {
 
-    this.props.addDataToState(this.state.htmltype, this.state.data)
-    //alert(" htmltype: " + this.state.htmltype + " data: " +this.state.data)
+      this.props.updateCurrentData(this.state.htmltype, this.state.data, this.props.id)
+
+      event.preventDefault();
+    } else {
+      this.props.addDataToState(this.state.htmltype, this.state.data)
+      this.setState({submitted: true})
+      event.preventDefault();
+    }
     event.preventDefault();
+
+
   }
 
   render() {
     return (
+
 
         <form onSubmit={this.handleSubmit}>
           <label>
             Content:
             <select value={this.state.htmltype} onChange={this.handleChange}>
               <option value="p">Paragraph</option>
-              <option value="header">Header</option>
+              <option value="h1">Header</option>
               <option value="other">other</option>
             </select>
-            <input type="text" value={this.state.data} onChange={this.handleDataChange} style={{width: "400px", height: "200px"}} />
+            <textarea value={this.state.data} onChange={this.handleDataChange} style={{width: "400px", height: "200px"}} />
           </label>
           <input type="submit" value="Submit" />
+
         </form>
 
 
