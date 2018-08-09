0f7e4b692115f3c8fe68ba2ac48ae73afe4521e2
Javier Zapata <zapataj87@gmail.com>
2018-08-09 01:47:52 -0500

    changed readme, added styling, added ways to add sections

--- src/FormType.js
+++ src/FormType.js
@@ -8,12 +8,20 @@ class FormType extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      hide: true,
       submitted: false,
       htmltype: 'h1',
       data: '',
     }
+    this.textArea = React.createRef();
 
   }
+
+  componentDidMount() {
+    this.textArea.current.focus()
+
+  }
+
 
   handleChange = (event) => {
       this.setState({htmltype: event.target.value})
@@ -35,15 +43,27 @@ class FormType extends React.Component {
       event.preventDefault();
     }
     event.preventDefault();
-
+    this.toggleEditField()
 
   }
 
+  toggleEditField = () => {
+    this.setState({hide: !this.state.hide})
+  }
+
+  focusTextArea = () => {
+    this.toggleEditField()
+  }
+
   render() {
+
+
     return (
 
+        <div>
+        { this.state.hide ?
 
-        <form onSubmit={this.handleSubmit}>
+          <form onSubmit={this.handleSubmit}>
           <label>
             Content:
             <select value={this.state.htmltype} onChange={this.handleChange}>
@@ -51,11 +71,16 @@ class FormType extends React.Component {
               <option value="h1">Header</option>
               <option value="other">other</option>
             </select>
-            <textarea value={this.state.data} onChange={this.handleDataChange} style={{width: "400px", height: "200px"}} />
+            <textarea ref={this.textArea} autoFocus={true} value={this.state.data}
+              onChange={this.handleDataChange}
+              style={{width: "400px", height: "200px"}} />
           </label>
           <input type="submit" value="Submit" />
-
         </form>
+
+        : (<this.state.htmltype onClick={this.focusTextArea}>{this.state.data}</this.state.htmltype>)
+}
+</div>
 
 
 
